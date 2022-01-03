adaa5c789c12ba1c5354527745d15a52dee76f4d
Hopper Gee <hopper.gee@hey.com>
2022-01-04 04:20:39 +0800

    Step 3 - create the confirmation page for confirmation code submit

--- app/controllers/two_factor_authentication/confirmations_controller.rb
+++ app/controllers/two_factor_authentication/confirmations_controller.rb
@@ -1,2 +1,14 @@
 class TwoFactorAuthentication::ConfirmationsController < ApplicationController
+  def create
+    if current_user.validate_and_consume_otp!(params.dig(:otp_code))
+      current_user.otp_required_for_login = true
+      current_user.save!
+  
+      render "two_factor_authentication/confirmations/success"
+    else
+      flash.now[:alert] = "Failed to confirm the 2FA code"
+      prepare_2fa_form
+      render :new
+    end
+  end
 end
