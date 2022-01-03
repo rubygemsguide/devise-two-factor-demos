4aad2ee31cc4f8e45eb1fbc146e0c3f7654edd8e
Hopper Gee <hopper.gee@hey.com>
2022-01-04 04:04:09 +0800

    Step 1 - Generate the dashboard page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
+
+  def show
+    redirect_to dashboard_path if user_signed_in?
+  end
 end
