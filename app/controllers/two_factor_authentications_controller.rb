f957c7f5a26a67fc6cb28963348a6ac04071e023
Hopper Gee <hopper.gee@hey.com>
2022-01-04 04:53:06 +0800

    Step 9: Disable 2FA

--- app/controllers/two_factor_authentications_controller.rb
+++ app/controllers/two_factor_authentications_controller.rb
@@ -9,4 +9,11 @@ class TwoFactorAuthenticationsController < ApplicationController
     @qrcode = current_user.otp_qrcode
     render 'two_factor_authentication/confirmations/new'
   end
+
+  def destroy
+    current_user.otp_required_for_login = false
+    current_user.otp_backup_codes&.clear
+    current_user.save!
+    redirect_to two_factor_authentication_path
+  end
 end
